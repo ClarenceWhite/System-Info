83ede39dd8516e9e4e8ec9ccbbc5fd546d6354b5
ClarenceWhite <48053038+ClarenceWhite@users.noreply.github.com>
2022-01-27 10:03:53 +0000

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+#setup
+setup(
+   name='simple.py',
+   version='1.0',
+   description='A useful module',
+   author='White',
+   author_email='zeyu.bai@ucdconnect.ie',
+   packages=['simple'],  #same as name
+   install_requires=[], #external packages as dependencies
+   license="MIT",
+   url="simple\__init__.py",
+    zip_safe=False
+)
